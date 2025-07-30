108c9b19601474317c17004ae8d3c2f0f87fbefb
Amran uddin <amranmetrouni@gmail.com>
2025-07-30 07:45:22 -0700

    update code

--- Problem #1.cpp
+++ Problem #1.cpp
@@ -1,1 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void mthorder (double lem)
+{
+     double thita,d;
+    cout <<"Enter value of angle (in degrees): ";
+    cin >>thita;
+
+    cout <<"Enter slit separation d (in µm): ";
+    cin>>d;
+
+    double tr=thita * 3.1416/180;
+    
+    double dnm = d * 1000; 
+
+    int m=round((dnm * sin(tr)) / lem);
+
+    cout <<"The final answer: "<<m<<" -th order maxima.";
+
+    }
+
+
+int main()
+{
+    double lemda; 
+
+    cout << "Enter value of wavelength (in nm): ";
+    cin >> lemda;
+
+    if (lemda <380|| lemda >750) 
+    {
+        cout <<"Out of the range. Please enter a valid number."<< endl;
+        
+        return 0;
+    }
+    else
+    {
+        mthorder(lemda);
+        
+    }
+    
+}
 
